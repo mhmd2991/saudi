550d81604cc4a50d805f2b89ae4435600b74179c
Mohamad Salimi <mohamad.salimi@hotmail.com>
2021-12-04 19:12:49 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+/*let tabs = document.querySelectorAll(".nav li");
+let tabsArray = Array.from(tabs);
+let divs = document.querySelectorAll(".content section");
+let divsArray = Array.from(divs);
+
+tabsArray.forEach((ele) => {
+    ele.addEventListener("click", function (e) {
+        tabsArray.forEach((ele) => {
+            ele.classList.remove("active");
+        });
+        e.currentTarget.classList.add("active");
+        divsArray.forEach((div) => {
+            div.classList.remove("active");
+        });
+        document.querySelector(e.currentTarget.dataset.cont).classList.add("active");
+    });
+});*/
+
+let radio = document.querySelectorAll("input");
+let radioArray = Array.from(radio);
+let tabs = document.querySelectorAll(".nav li");
+let tabsArray = Array.from(tabs);
+
+
+tabsArray.forEach((ele) => {
+    ele.addEventListener("click", function (e) {
+        tabsArray.forEach((ele) => {
+            ele.classList.remove("active");
+        });
+        e.currentTarget.classList.add("active");
+       document.querySelector(e.currentTarget.dataset.cont).checked = true;
+    });
+});
+
 
